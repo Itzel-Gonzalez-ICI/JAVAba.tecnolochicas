01a08446efba8ad38acd395439d5ab7cb840f4ab
Valeria Itzel Trinidad González <valeria.itzel_tg@hotmail.com>
2025-05-16 12:24:34 -0600

    Add files via upload
    addd s s rts

---
+++ JAVA básico/sesiones/Sesión 1/RETOO 01/src/reto1/Paciente.java
@@ -0,0 +1,25 @@
+package reto1;
+import java.lang.System;
+
+public class Paciente {
+    //atributos
+    private String name;
+    private int age;
+    private String numExpediente;
+
+    // constructor
+    public Paciente(String name, int age, String numExpediente) {
+        this.name = name;
+        this.age = age;
+        this.numExpediente = numExpediente;
+    }
+
+    //metodo para mostrar la informacion del paciente
+    public void MostrarInformacion() {
+        System.out.printf("""
+                Nombre del paciente: %s.
+                Edad del paciente: %d.
+                Numero de expediente: %s.""", name, age, numExpediente);
+    }
+}
+
